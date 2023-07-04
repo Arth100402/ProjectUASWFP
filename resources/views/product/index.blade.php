4fc7eccb0de62a44d2dc2c4f245b962461f7ffcd
Talahatu <87047371+Talahatu@users.noreply.github.com>
2023-07-04 19:25:23 +0700

    Update index.blade.php

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,10 +1,11 @@
 @extends('utama')
 @section('isi')
-@if (session('status'))
-<div class="alert alert-success">{{session('status')}}</div>
-@endif
-<!DOCTYPE html>
-<html lang="en">
+    @if (session('status'))
+        <div class="alert alert-success">{{ session('status') }}</div>
+    @endif
+    <!DOCTYPE html>
+    <html lang="en">
+
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -14,18 +15,21 @@
         <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet" />
     </head>
+
     <body>
-    <a href="{{route('product.create')}}" class="btn btn-success">+ New Products</a><p>
-        <!-- Navigation-->
-        <!-- Header-->
+        <a href="{{ route('product.create') }}" class="btn btn-success">+ New</a>
+        <p>
+            <!-- Navigation-->
+            <!-- Header-->
         <header class="bg-dark py-5">
-                <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">Find Your Skincare, Make-up, and Outfit</p>
+            <div class="text-center text-white">
+                <h1 class="display-4 fw-bolder">Shop in style</h1>
+                <p class="lead fw-normal text-white-50 mb-0">Find Your Skincare, Make-up, and Outfit</p>
             </div>
         </header>
         <!-- Section-->
@@ -33,40 +37,46 @@
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
 
-                    @foreach($data as $d)
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Product image-->
-                            <img class="card-img-top" style="object-fit: cover;" src="{{ asset('images/'.$d->image) }}" height='200px'/>
-                            <!-- Product details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Product name --> 
-                                    <h5 class="fw-bolder"><a href="{{ route('product.show',$d->id) }}">{{$d->name}}</a></h5>
-                                    <!-- Product price-->
-                                    Rp. {{$d->price}}
-                                    <br>
-                                    <a href="{{ route('product.edit',$d->id) }}">Ubah</a>
+                    @foreach ($data as $d)
+                        <div class="col mb-5">
+                            <div class="card h-100">
+                                <!-- Product image-->
+                                <img class="card-img-top" style="object-fit: cover;" src="{{ asset('images/' . $d->image) }}"
+                                    height='200px' />
+                                <!-- Product details-->
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        <!-- Product name -->
+                                        <h5 class="fw-bolder"><a
+                                                href="{{ route('product.show', $d->id) }}">{{ $d->name }}</a></h5>
+                                        <!-- Product price-->
+                                        Rp. {{ $d->price }}
+                                        <br>
+                                        <a href="{{ route('product.edit', $d->id) }}">Ubah</a>
+                                    </div>
+                                </div>
+                                <!-- Product actions-->
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto"
+                                            href="{{ route('addToCart', $d->id) }}">Add To Cart</a></div>
                                 </div>
                             </div>
-                            <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="{{route('addToCart',$d->id)}}">Add To Cart</a></div>
-                            </div>
                         </div>
-                    </div>
                     @endforeach
-                    </div>
+                </div>
             </div>
         </section>
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Complete your gorgeous clothing collection from us</p></div>
+            <div class="container">
+                <p class="m-0 text-center text-white">Complete your gorgeous clothing collection from us</p>
+            </div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
     </body>
-</html>
+
+    </html>
 @endsection
